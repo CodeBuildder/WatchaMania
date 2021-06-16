eac30c6a26b1973f1c22ceecd1e7dc6e3d6cda6d
Codebuilder2022 <kumarankaushik@gmail.com>
2021-06-16 23:26:24 +0400

    ✨

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
-import apiRequests from "./api/api";
 import Row from "./Components/Row";
 import apiRequest from "./api/api";
 function App() {
   return (
     <div className="App">
       Hey
-      <Row title="NETFLIX ORIGINALS" url={apiRequest.fetchNetflixOriginals} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        url={apiRequest.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending Now" url={apiRequest.fetchTrending} />
+      <Row title="Top Rated" url={apiRequest.fetchTrending} />
+      <Row title="Action Movies" url={apiRequest.fetchActionMovies} />
+      <Row title="Comedy Movies" url={apiRequest.fetchComedyMovies} />
+      <Row title="Horrow Movies" url={apiRequest.fetchHorrorMovies} />
+      <Row title="Romance Movies" url={apiRequest.fetchRomanceMovies} />
+      <Row title="Documentaries" url={apiRequest.fetchDocumentaries} />
     </div>
   );
 }
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,20 +1,39 @@
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
+import "./Row.css";
 
-function Row({ title, url }) {
+const imageURL = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, url, isLargeRow }) {
   const [movie, setMovie] = useState([]);
 
   useEffect(() => {
     async function fetchMovie() {
       const request = await axios.get(url);
-      console.log(request);
+      setMovie(request.data.results);
+      console.log(request.data.resultsyea);
     }
     fetchMovie();
-  }, []);
+  }, [url]);
 
+  //console.log(movie);
   return (
-    <div>
+    <div className="row">
       <h2> {title} </h2>
+      <div className="flex w-full p-7 object-contain overflow-y-hidden overflow-x-scroll moviePoster">
+        {movie.map((movie) => (
+          <img
+            key={movie.id}
+            src={`${imageURL}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            className={`mr-3 max-h-32 cursor-pointer row-movie ${
+              isLargeRow && "max-h-60 hover:scale-120"
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
